eb2ca5c594f695aea5a217592d6c7200a759131e
Zhao Kaili <1395669489@qq.com>
2019-12-06 15:34:41 +0800

    三四次

--- Exercise/Zhaokaili.R
+++ Exercise/Zhaokaili.R
@@ -1,5 +1,5 @@
 library(tseries)
-setwd("D:/时间序列/习题数据、案例数据、R代码")
+setwd("E:/github_repo/time_series")
 
 #自定义求自相关系数的函数auto_acf
 auto_acf <- function(ts,k){
@@ -64,4 +64,153 @@ for (i in 1:2) print(Box.test(Yt, lag=i*6))
 #所以认为Yt是白噪声序列
 
 
+#######################第三次上机##################################
+rm(list = ls())
+#模型1:xt=0.5*xt-1 + et
 
+n <- 100
+phi1 <- 0.5
+x1 <- arima.sim(n=n,list(ar=phi1)) #模拟模型1
+ts.plot(x1) #序列平稳
+G1 <- c()
+G1[1] <- 1
+G1[2] <- phi1*G1[1]
+for (i in 3:n){
+  G1[i]=phi1*G1[i-1]
+}
+G1[1:10] #格林函数前10项
+
+varx1 <- sum(G1^2*1)
+varx1 #方差
+
+gamma <- c()
+gamma[1] <- varx1
+gamma[2] <- phi1*gamma
+for (i in 3:10){
+  gamma[i]=phi1*gamma[i-1]
+}
+gamma #协方差函数前10项
+
+rhox1 <- gamma[1:10]/varx1
+rhox1 #自相关系数前10项
+
+##模型2：xt=xt-1 - -0.5*xt-2 + et
+phi1 <- 1
+phi2 <- -0.5
+x2 <- arima.sim(n=100,list(ar=c(phi1,phi2))) #模拟模型2
+ts.plot(x2) #该序列平稳
+
+G2 <- c()
+G2[1] <- 1
+G2[2]<- phi1*G2[1]
+for (i in 3:n){
+  G2[i]=phi1*G2[i-1]+phi2*G2[i-2]
+}
+G2[1:10] #格林函数前10项
+
+varx2 <- sum(G2^2*1)
+varx2 #方差
+
+gamma <- c()
+gamma[1] <- varx2
+gamma[2] <- phi1*gamma[1]/(1-phi2)
+for (i in 3:10) {
+  gamma[i]=phi1*gamma[i-1]+phi2*gamma[i-2]
+}
+gamma #协方差函数前10项
+
+rhox2 <- gamma[1:10]/varx2
+rhox2 #自相关系数前10项
+
+##模型3：xt=-xt-1 - -0.5*xt-2 + et
+phi1 <- 0.5
+phi2 <- -0.1
+x3 <- arima.sim(n=100,list(ar=c(phi1,phi2))) #模拟模型3
+ts.plot(x3) #该序列平稳
+
+G3 <- c()
+G3[1] <- 1
+G3[2] <- phi1*G3[1]
+for (i in 3:n){
+  G3[i]=phi1*G3[i-1]+phi2*G3[i-2]
+}
+G3[1:10] #格林函数前10项
+
+varx3 <- sum(G3^2*1)
+varx3 #方差
+
+gamma <- c()
+gamma[1] <- varx3
+gamma[2] <- phi1*gamma[1]/(1-phi2)
+for (i in 3:10) {
+  gamma[i]=phi1*gamma[i-1]+phi2*gamma[i-2]
+}
+gamma #协方差函数前10项
+
+rhox3 <- gamma[1:10]/varx3
+rhox3 #自相关系数前10
+
+
+#######################第四次上机##################################
+phi <- c(0.1,0.2,-0.3,0.4)
+theta <- c(0.6,0.7,0.8)
+#ARMA(3,4)转化为AR(100)
+I <- c()
+I[1] <- 1
+I[2] <- theta[1]
+I[3] <- theta[1]*I[2]+theta[2]
+for (i in 4:100)  {
+  I[i]<-sum(theta*I[(i-1):(i-3)])
+}
+I[2:5] <- I[2:5]-phi
+I
+
+###ARMA(3,4)转化为MA(100)
+G <- c()
+G[1] <- 1
+G[2] <- phi[1]
+G[3] <- phi[1]*G[2]+phi[2]
+G[4] <- phi[1]*G[3]+phi[2]*G[2]+phi[3]
+for (i in 5:100)  {
+  G[i]<-sum(phi*G[(i-1):(i-4)])
+}
+G[2:4] <- G[2:4]-theta
+G
+
+
+###写出MA(4)的ACF,根为0.2，0.3,0.6，0.9
+library(Ryacas)
+yacas("Expand((x-0.2)*(x-0.3)*(x-0.6)*(x-0.9))")
+theta2 <- c(-2,1.35,-0.36,0.0324) #由上面表达式可得
+ma_acf <- c()
+ma_acf[1] <- 1
+ma_acf[5] <- theta2[4]/(1+sum(theta2^2))
+for( i in 2:4){
+  ma_acf[i]=(theta2[i-1]+sum(theta2[1:(5-i)]*theta2[i:4]))/(1+sum(theta2^2))
+}
+ma_acf
+ARMAacf(ma=theta2, lag.max = 4)#检验,结果一致
+
+##写出AR(5)的PACF，根为0.25，0.5，0.6，0.75，0.8
+yacas("Expand((x-0.25)*(x-0.5)*(x-0.6)*(x-0.75)*(x-0.8))")
+phi2 <- c(2.9,-3.2675,1.77625,-0.46125,0.045)
+ar_acf <- ARMAacf(ar=phi2,lag.max = 6)
+ar_acf
+shift <- function(v,k){
+  if (k==0) v else c(tail(v,k),head(v,-k))
+}
+shift <- Vectorize(shift, "k")
+ar_pacf <- function(k){
+  for (i in 1:k){
+  v <- ar_acf[1:i]
+  temp <- shift(v, 0:(i-1)) #自相关系数矩阵
+  temp[upper.tri(temp)] <- 0 #化为下三角矩阵
+  D <- temp+t(temp) #化为对称矩阵
+  diag(D) <- 1  
+  Dk = D
+  Dk[,i] <-AR.ACF[2:(i+1)] 
+  ar_pacf <- det(Dk)/det(D)
+  print(ar_pacf)}
+}
+ar_pacf(5) #AR(5)的PACF
+ARMAacf(ar=phi2, lag.max = 5, pacf = T) #检验
