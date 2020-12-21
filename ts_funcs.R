2e5133f681fc14c633196926413eaf7a7cfafd81
Liao Mingke <951368155@qq.com>
2020-12-21 19:01:32 +0800

    fix

--- ts_funcs.R
+++ ts_funcs.R
@@ -1,4 +1,4 @@
-# 函数一：计算时间序列x延迟第k期的自相关系数ACF，并在Q.test和LB.test中使用
+# 函数一：计算时间序列x延迟第k期的自相关系数ACF
 ACF = function(x,k){
     n = length(x)
     x_bar = mean(x)
@@ -8,8 +8,6 @@ ACF = function(x,k){
 
 # 函数二：计算时间序列x延迟前k期的所有自相关系数ACFs
 ACFs = function(x,k){
-    n = length(x)
-    x_bar = mean(x)
     rou = c()
     for(i in 1:k) rou[i] = ACF(x,i)
     return(rou)
@@ -20,7 +18,7 @@ Q.test = function(x,k){
     n = length(x)
     rou_k = ACFs(x,k)
     Q = n*sum(rou_k^2)
-    p.v = 1 - pchisq(Q, df=k)
+    p.v = 1 - pchisq(Q, df = k)
     result = c(Q, p.v)
     return(data.frame(result,row.names = c('x-squared', 'p-value')))
 }
